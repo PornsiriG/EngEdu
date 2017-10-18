8e5b28f12503a9fcdb6615f8de6820a1eed4fe12
PornsiriG <58030196@kmitl.ac.th>
2017-10-18 16:45:12 +0700

    2nd  year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -13,6 +13,9 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 1, I am Engedu Freshman.");
             Console.WriteLine("Year 255x semester 2, I learn to program in C sharp.");
             Console.WriteLine("Year 2560 semester 1, I have to regrade on some course.");
+            Console.WriteLine("Year 2560 semester 2, I study in computer engineering.");
+           
+            
         }
     }
 }
